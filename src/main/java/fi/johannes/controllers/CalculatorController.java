842e2d39ca918ba13cde498a548a400132942b38
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2016-11-06 16:45:40 +0200

    bug with controller

--- src/main/java/fi/johannes/controllers/CalculatorController.java
+++ src/main/java/fi/johannes/controllers/CalculatorController.java
@@ -3,17 +3,19 @@ package fi.johannes.controllers;
 import java.math.BigInteger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import fi.johannes.dto.MathOperationData;
 import fi.johannes.services.ConversionService;
 import fi.johannes.services.MathService;
 
-@RestController
+// XXX Note to self, RestController does not work with ThymeLeaf and will result only string to be returned
+// as is, not as a template
+@Controller
 public class CalculatorController {
 	
 	@Autowired
