fe3722fcd1bf603121d2426e5db40316e24d4707
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2016-11-07 22:58:40 +0200

    configured db

--- src/main/java/fi/johannes/MoocDemoiluApplication.java
+++ src/main/java/fi/johannes/MoocDemoiluApplication.java
@@ -1,12 +1,22 @@
 package fi.johannes;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MoocDemoiluApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoocDemoiluApplication.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+	    registration.addUrlMappings("/console/*");
+	    return registration;
 	}
 }
