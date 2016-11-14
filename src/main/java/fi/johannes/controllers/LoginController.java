c0f3cb69203557a1585045abcee6a76325efb0ae
Johannes Sarpola <johannes.sarpola@gmail.com>
2016-11-14 20:33:43 +0200

    started login controller

---
+++ src/main/java/fi/johannes/controllers/LoginController.java
@@ -0,0 +1,16 @@
+package fi.johannes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping(path="/login")
+public class LoginController {
+
+	@RequestMapping(path="/", method=RequestMethod.GET)
+	String loginPage(){
+		return "this is login";
+	}
+}
