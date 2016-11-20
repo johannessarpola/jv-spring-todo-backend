161aec306f8ab5417ac988c0a73ea0d10d9a4d95
Johannes Sarpola <johannes.sarpola@gmail.com>
2016-11-20 18:50:54 +0200

    Fixed to be controller

--- src/main/java/fi/johannes/controllers/AppController.java
+++ src/main/java/fi/johannes/controllers/AppController.java
@@ -3,6 +3,7 @@ package fi.johannes.controllers;
 import fi.johannes.clients.TodoClient;
 import fi.johannes.entity.Todo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -10,17 +11,18 @@ import java.util.List;
 /**
  * Created by johanness on 18/11/2016.
  */
+@Controller
 public class AppController {
     @Autowired
     private TodoClient todoClient;
 
     @RequestMapping("/")
-    String index(){
+    public String index(){
         // TODO welcome page
         return "index";
     }
     @RequestMapping("/week")
-    String weekTodos(){
+    public String weekTodos(){
 
         return "";
     }
