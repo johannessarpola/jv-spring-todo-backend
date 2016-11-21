c85df717e0e62541ff5da084cfae25beec34ead6
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2016-11-21 21:06:12 +0200

    added some population to db method

--- src/main/java/fi/johannes/TodoApp.java
+++ src/main/java/fi/johannes/TodoApp.java
@@ -1,12 +1,19 @@
 package fi.johannes;
 
+import fi.johannes.entity.Todo;
+import fi.johannes.entity.User;
+import fi.johannes.services.TodoService;
 import org.h2.server.web.WebServlet;
 import org.h2.tools.Server;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.Random;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -34,5 +41,34 @@ public class TodoApp {
 	    }
 	    return server;
 
+	}
+
+	@Bean
+	public CommandLineRunner populateWeek(TodoService todoService){
+        return (args) -> {
+            String[] entries = {"Wash dishes", "Take out garbages", "Walk the dog", "Study stuff"};
+            String[] logins = {"johnny", "ben", "einstein"};
+            String[] emails = {"johnny@mail.ru", "ben@bensmail.com", "one@ones.com"};
+            String[] names = {"Johnny Alamo", "Ben Rubenstein", "Albert Einstein"};
+            Random random = new Random(123L);
+            for (int i = 0; i < 20; i++) {
+                Todo todo = new Todo();
+                User user = new User();
+                int userindex = random.nextInt(logins.length - 1);
+                int entryindex = random.nextInt(entries.length - 1);
+                LocalDateTime now = LocalDateTime.now().withHour(6);
+                int secondstoadd = random.nextInt(7 * 24 * 60 * 60);
+                int secondstoaddcreated = random.nextInt(12 * 60 * 60);
+                user.setEmail(emails[userindex]);
+                user.setName(names[userindex]);
+                user.setLogin(logins[userindex]);
+                todo.setCreator(user);
+                todo.setDone(random.nextBoolean());
+                todo.setEntry(entries[entryindex]);
+                todo.setDeadline(now.plusSeconds(secondstoadd));
+                todo.setCreated(now.plusSeconds(secondstoaddcreated));
+                todoService.store(todo);
+            }
+        };
 	}
 }
