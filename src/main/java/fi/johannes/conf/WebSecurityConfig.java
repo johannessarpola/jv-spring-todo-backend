05a996d7f3b8d0e4df9adb50ae4efc0dad044469
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2016-12-14 18:40:41 +0200

    modified sec

--- src/main/java/fi/johannes/conf/WebSecurityConfig.java
+++ src/main/java/fi/johannes/conf/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package fi.johannes.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +12,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
