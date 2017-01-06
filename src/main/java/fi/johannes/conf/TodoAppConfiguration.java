34791ba7111ec0e135fe3f07fba3bb8e45826a51
Johannes Sarpola <johannes.sarpola@gmail.com>
2017-01-06 19:02:34 +0200

    Config but does not work on deser

--- src/main/java/fi/johannes/conf/TodoAppConfiguration.java
+++ src/main/java/fi/johannes/conf/TodoAppConfiguration.java
@@ -2,6 +2,8 @@ package fi.johannes.conf;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -12,11 +14,14 @@ import fi.johannes.serialization.LocalDateTimeSerializer;
 @Configuration
 public class TodoAppConfiguration {
 
+    // For some reason serializer works but deserializer won't
 	@Bean
-	public Jackson2ObjectMapperBuilder jacksonBuilder() {
-		return new Jackson2ObjectMapperBuilder()
-				// Custom serializers for LocalDateTime
-				.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer())
-				.deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer());
+	public Jackson2ObjectMapperBuilderCustomizer addCustomDateSerialization() {
+		return jacksonObjectMapperBuilder -> {
+            jacksonObjectMapperBuilder
+                .serializerByType(LocalDateTime.class, new LocalDateTimeSerializer());
+            jacksonObjectMapperBuilder
+                .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer());
+        };
 	}
 }
