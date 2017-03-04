b019b40da31529bcda5383f76f21b3c275fdd845
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-03-04 15:53:17 +0200

    Modified the login form

--- src/main/java/fi/johannes/controllers/LoginController.java
+++ src/main/java/fi/johannes/controllers/LoginController.java
@@ -3,18 +3,23 @@ package fi.johannes.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 
 @Controller
 public class LoginController {
-	
 
-	@RequestMapping(path="/login", method=RequestMethod.GET)
-	String loginPage(){
-		return "login";
-	}
 	@RequestMapping(path="/logout", method=RequestMethod.GET)
 	String logout(){
 		return "logout";
+	}
+
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView getLoginPage(@RequestParam(required = false) String error) {
+		return new ModelAndView("login", "error", Optional.ofNullable(error));
 	}
 }
