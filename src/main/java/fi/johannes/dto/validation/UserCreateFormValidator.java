9323ec7eac2e6053aef57d1a8009e6feddd92a52
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-03-05 20:33:20 +0200

    form validator

---
+++ src/main/java/fi/johannes/dto/validation/UserCreateFormValidator.java
@@ -0,0 +1,48 @@
+package fi.johannes.dto.validation;
+
+import fi.johannes.dto.UserCreateForm;
+import fi.johannes.services.interfaces.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserCreateFormValidator implements Validator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserCreateFormValidator.class);
+    private final UserService userService;
+
+    @Autowired
+    public UserCreateFormValidator(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.equals(UserCreateForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        LOGGER.debug("Validating {}", target);
+        UserCreateForm form = (UserCreateForm) target;
+        validatePasswords(errors, form);
+        validateEmail(errors, form);
+    }
+
+    private void validatePasswords(Errors errors, UserCreateForm form) {
+        if (!form.getPassword().equals(form.getPasswordRepeated())) {
+            errors.reject("password.no_match", "Passwords do not match");
+        }
+    }
+
+    private void validateEmail(Errors errors, UserCreateForm form) {
+        // TODO Add check only one email
+        if (userService.getUserByLogin(form.getLogin()).isPresent()) {
+            errors.reject("email.exists", "User with this login already exists");
+        }
+    }
+}
