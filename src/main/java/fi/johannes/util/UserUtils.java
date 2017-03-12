e5ed4c006fb2ca6a226283b87eecc19fb18ae993
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-03-12 17:45:01 +0200

    Reverted the port change and added some methods to userutils

--- src/main/java/fi/johannes/util/UserUtils.java
+++ src/main/java/fi/johannes/util/UserUtils.java
@@ -2,16 +2,40 @@ package fi.johannes.util;
 
 import fi.johannes.models.CustomUserDetails;
 import fi.johannes.models.User;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.ldap.userdetails.LdapUserDetails;
+
+import java.security.Principal;
 
 /**
  * johanness on 07/03/2017.
  */
 public class UserUtils {
 
-    public static User getCurrentUser(){
-        CustomUserDetails userDetails =
-                (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public static UserDetails getUserDetails(){
+        return (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+    public static CustomUserDetails getCustomUserDetails(){
+        return (CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public static User getCustomUser(){
+        CustomUserDetails userDetails = getCustomUserDetails();
         return userDetails.getUser();
     }
+    public static Authentication getAuthentication(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+    private static Object getUserObj(){
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+    public static boolean isLdapAuthenticated(){
+        return getUserObj() instanceof LdapUserDetails;
+    }
+    public static boolean isInternallyAuthenticated(){
+        return !isLdapAuthenticated();
+    }
+
 }
