afa41467f5d051bdbf872dd0f19569b2c293ec8c
Johannes Sarpola <johannes.sarpola@ambientia.fi>
2017-03-15 11:30:15 +0200

    started with keywords collector

---
+++ src/main/java/fi/johannes/util/collectors/KeywordCollector.java
@@ -0,0 +1,44 @@
+package fi.johannes.util.collectors;
+
+import fi.johannes.models.Word;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+/**
+ * johanness on 15/03/2017.
+ */
+public class KeywordCollector implements Collector<String, Set<Word>, Word> {
+    @Override
+    public Supplier<Set<Word>> supplier() {
+        return () -> new HashSet<>();
+    }
+
+    @Override
+    public BiConsumer<Set<Word>, String> accumulator() {
+        return (set, str) -> set.add(new Word(str));
+    }
+
+    @Override
+    public BinaryOperator<Set<Word>> combiner() {
+        return (a, b) -> {
+            a.addAll(b);
+            return a;
+        }
+    }
+
+    @Override
+    public Function<Set<Word>, Word> finisher() {
+        return null;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return null;
+    }
+}
