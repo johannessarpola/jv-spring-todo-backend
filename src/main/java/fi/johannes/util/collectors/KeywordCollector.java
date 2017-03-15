06fc9a91ec983cf59f7fe1af7f33e783b1fbf7e5
Johannes Sarpola <johannes.sarpola@gmail.com>
2017-03-15 23:21:16 +0200

    Collector for kw and test

--- src/main/java/fi/johannes/util/collectors/KeywordCollector.java
+++ src/main/java/fi/johannes/util/collectors/KeywordCollector.java
@@ -1,9 +1,12 @@
 package fi.johannes.util.collectors;
 
 import fi.johannes.models.Word;
+import fi.johannes.services.repositories.WordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
@@ -13,32 +16,32 @@ import java.util.stream.Collector;
 /**
  * johanness on 15/03/2017.
  */
-public class KeywordCollector implements Collector<String, Set<Word>, Word> {
-    @Override
-    public Supplier<Set<Word>> supplier() {
-        return () -> new HashSet<>();
+public class KeywordCollector {
+
+    private WordRepository wordRepository;
+
+    @Autowired
+    public KeywordCollector(WordRepository wordRepository) {
+        this.wordRepository = wordRepository;
     }
 
-    @Override
-    public BiConsumer<Set<Word>, String> accumulator() {
-        return (set, str) -> set.add(new Word(str));
+    private Supplier<HashSet<Word>> supplier = HashSet::new;
+    private BiConsumer<HashSet<Word>, String> accumulator = (set, str) -> set.add(saveOrGet(str));
+    private BinaryOperator<HashSet<Word>> combiner = (left, right) -> { left.addAll(right); return left; };
+    private Function<HashSet<Word>, HashSet<Word>> finisher = words -> words;
+
+    private Word saveOrGet(String str){
+        Word w = wordRepository.findOneByWordStr(str).orElseGet(() -> wordRepository.save(new Word(str)));
+        return w;
     }
 
-    @Override
-    public BinaryOperator<Set<Word>> combiner() {
-        return (a, b) -> {
-            a.addAll(b);
-            return a;
-        };
+    public Collector<String, HashSet<Word>, HashSet<Word>> getCollector(){
+        return new CollectorImpl<>(supplier, accumulator, combiner, finisher, Collections.emptySet());
     }
 
-    @Override
-    public Function<Set<Word>, Word> finisher() {
-        return null;
-    }
-
-    @Override
-    public Set<Characteristics> characteristics() {
-        return null;
+    private EnumSet<Collector.Characteristics> getDefParallelCollection(){
+        return EnumSet.of(
+                Collector.Characteristics.CONCURRENT,
+                Collector.Characteristics.IDENTITY_FINISH);
     }
 }
---
+++ src/test/java/fi/johannes/util/collectors/KeywordCollectorTest.java
@@ -0,0 +1,44 @@
+package fi.johannes.util.collectors;
+
+import fi.johannes.models.Word;
+import fi.johannes.services.repositories.WordRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collector;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataJpaTest
+public class KeywordCollectorTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private WordRepository wordRepository;
+
+    @Test
+    public void testCollector() throws Exception {
+        this.entityManager.persist(new Word("C"));
+        assertTrue(wordRepository.findOneByWordStr("C") != null);
+        KeywordCollector kw = new KeywordCollector(wordRepository);
+        List<String> strings = Arrays.asList("A", "B", "C");
+        Collector<String, HashSet<Word>, HashSet<Word>> kwCollector = kw.getCollector();
+        HashSet<Word> wordHashSet = strings.stream().collect(kwCollector);
+        assertEquals(strings.size(), wordHashSet.size());
+        assertTrue(wordRepository.findAll().stream().anyMatch(word -> strings.contains(word.getWordStr())));
+    }
+
+}
